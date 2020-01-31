4e99e5f0213b41c1ce4bf8ff2ae03c90f3561b29
聂淼 <905336559@qq.com>
2020-01-31 15:52:12 +0100

    页面信息查询

---
+++ xc-service-api/src/main/java/com/xuecheng/api/cms/CmsPageControllerApi.java
@@ -0,0 +1,33 @@
+package com.xuecheng.api.cms;
+
+import com.xuecheng.framework.domain.cms.request.QueryPageRequest;
+import com.xuecheng.framework.model.response.QueryResponseResult;
+import com.xuecheng.framework.utils.MD5Util;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+
+/**
+ * @description: 页面管理接口
+ * @author: Niem
+ * @Date: 2020/1/31-10:34
+ */
+@Api(value="cms页面管理接口",description = "cms页面管理接口，提供页面的增、删、改、查")
+public interface CmsPageControllerApi {
+
+
+    /**
+    * @Description: 页面查询
+    * @Param: [page, size, queryPageRequest]
+    * @Return: com.xuecheng.framework.model.response.QueryResponseResult
+    * @Date: 2020/1/31
+    */
+    @ApiOperation("分页查询页面列表")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name="page",value = "页码",required=true,paramType="path",dataType="int"),
+            @ApiImplicitParam(name="size",value = "每页记录数",required=true,paramType="path",dataType="int")
+    })
+    public QueryResponseResult findList(int page, int size, QueryPageRequest queryPageRequest) ;
+
+}
