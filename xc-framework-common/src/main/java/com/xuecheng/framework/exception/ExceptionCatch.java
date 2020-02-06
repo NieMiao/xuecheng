fa946e9801915f7340fc85b8afa121b51d41ae00
聂淼 <905336559@qq.com>
2020-02-06 14:41:46 +0100

    其他异常捕获

--- xc-framework-common/src/main/java/com/xuecheng/framework/exception/ExceptionCatch.java
+++ xc-framework-common/src/main/java/com/xuecheng/framework/exception/ExceptionCatch.java
@@ -1,9 +1,12 @@
 package com.xuecheng.framework.exception;
 
+import com.google.common.collect.ImmutableMap;
+import com.xuecheng.framework.model.response.CommonCode;
 import com.xuecheng.framework.model.response.ResponseResult;
 import com.xuecheng.framework.model.response.ResultCode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -15,7 +18,36 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class ExceptionCatch {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ExceptionCatch.class);
-    
+
+    //使用EXCEPTIONS存放异常类型和错误代码的映射，ImmutableMap的特点的一旦创建不可改变，并且线程安全
+    private static ImmutableMap<Class<? extends Throwable>,ResultCode> EXCEPTIONS;
+    //使用builder来构建一个异常类型和错误代码的异常
+    protected static ImmutableMap.Builder<Class<? extends Throwable>,ResultCode> builder = ImmutableMap.builder();
+
+
+
+    /**
+    * @Description: 捕获Exception异常
+    * @Param: [e]
+    * @Return: com.xuecheng.framework.model.response.ResponseResult
+    * @Date: 2020/2/6
+    */
+    @ResponseBody
+    @ExceptionHandler(Exception.class)
+    public ResponseResult exception(Exception e) {
+        LOGGER.error("catch exception : {}\r\nexception: "
+                ,e.getMessage(), e);
+        if(EXCEPTIONS == null)
+            EXCEPTIONS = builder.build();
+        final ResultCode resultCode = EXCEPTIONS.get(e.getClass());
+        final ResponseResult responseResult;
+        if (resultCode != null) {
+            responseResult = new ResponseResult(resultCode);
+        } else {
+            responseResult = new ResponseResult(CommonCode.SERVER_ERROR);
+        }
+        return responseResult;
+    }
     /**
     * @Description: 捕获统一异常
     * @Param: [e]
@@ -29,5 +61,10 @@ public class ExceptionCatch {
                 ,e.getMessage(), e);
         ResultCode resultCode = e.getResultCode();
         return new ResponseResult(resultCode);
+    }
+
+    static{
+     //在这里加入一些基础的异常类型判断
+        builder.put(HttpMessageNotReadableException.class,CommonCode.INVALID_PARAM);
     }
 }
